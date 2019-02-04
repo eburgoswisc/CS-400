052e9968c154da42357ca413c8ea6093b80655f3
Emanuel Burgos <eburgos@wisc.edu>
2019-02-04 10:01:39 -0600

    Update CS400 notes and P1

--- assignments/p1/DS_My.java
+++ assignments/p1/DS_My.java
@@ -1,56 +1,87 @@
 
 public class DS_My implements DataStructureADT {
+
+    // TODO may wish to define an inner class 
+    // for storing key and value as a pair
+    // such a class and its members should be "private"
 	
-	private Pair[] items;
+	private class Pair  {
+		private Comparable key;
+		private Object value;
+		
+		private Pair(Comparable k, Object v) {
+			this.key = k;
+			this.value = v;
+		}
+	}
+	
+	private Pair[] ls;
 	private int size;
-
-    private class Pair<K, V> {
-    	private K key;
-    	private V value;
-    	
-    	private Pair(K key, V value) {
-    		this.key = key;
-    		this.value = value;
-    	}
-    	
-    }
-
-    // Private Fields of the class
-    // TODO create field(s) here to store data pairs
     
     public DS_My() {
-        this.items = new Pair[500];
+        this.ls = new Pair[501];
         this.size = 0;
     }
 
     @Override
     public void insert(Comparable k, Object v) {
-        items[size] = new Pair<t>(k,v);
+        if (this.size == 0) {
+        	Pair p = new Pair(k,v);
+        	ls[this.size] = p;
+        	this.size++;
+        
+        return;
+        }
+        else {
+        	for (int i = 0; i < this.size; i++) {
+        		if (ls[i].key.equals(k)) {throw new RuntimeException();}
+        	}
+        	Pair p = new Pair(k,v);
+        	ls[this.size] = p;
+        	this.size++; 	
+        }
         
     }
 
     @Override
     public boolean remove(Comparable k) {
-        // TODO Auto-generated method stub
-        return false;
+    	Pair[] newList = new Pair[500];
+    	boolean found = false;
+    	for (int i = 0; i < this.size; i++) {
+    		if (ls[i].key.equals(k)) {
+    			found = true;
+    			size--;
+    			continue;
+    		}
+    	}
+    	this.ls = newList;
+        return found;
     }
 
     @Override
     public boolean contains(Comparable k) {
-        // TODO Auto-generated method stub
+    	for (int i = 0; i < this.size; i++) {
+    		if (ls[i].key.equals(k)) {
+    			return true;
+    		}
+    	}
         return false;
     }
 
     @Override
     public Object get(Comparable k) {
-        // TODO Auto-generated method stub
-        return null;
+    	for (int i = 0; i < this.size; i++) {
+    		if (ls[i].key.equals(k)) {
+    			return ls[i].value;
+    		}
+    	}
+		return null;
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.size;
     }
 
 }
+
--- assignments/p1/DataStructureADTTest.java
+++ assignments/p1/DataStructureADTTest.java
@@ -40,12 +40,43 @@ abstract class DataStructureADTTest<T extends DataStructureADT<String,String>> {
 	// TODO: implement tests 01 - 04
 
 	// test01_after_insert_one_size_is_one
+	@Test
+	void test01_after_insert_one_size_is_one() {
+		dataStructureInstance.insert("Key1", "Value1");
+		
+		if (dataStructureInstance.size() != 1) {
+			fail("data structure should have 1 item, but size was " + dataStructureInstance.size());
+		}
+	}
 	
 	// test02_after_insert_one_remove_one_size_is_0
+	@Test
+	void test02_after_insert_one_remove_one_size_is_0() {
+		dataStructureInstance.insert("Key1", "Value1");
+		
+		dataStructureInstance.remove("Key1");
+		
+		assertEquals(dataStructureInstance.size(), 0);
+	}
 	
 	// test03_duplicate_exception_is_thrown
+	@Test
+	void test03_duplicate_exception_is_thrown() {
+		dataStructureInstance.insert("Key1", "Value1");
+		try {
+			dataStructureInstance.insert("Key1", "Value1");
+		} catch (RuntimeException e) {} // Do nothing if passes
+	}
 	
 	// test04_remove_returns_false_when_key_not_present
+	@Test
+	void test04_remove_returns_false_when_key_not_present() {
+		dataStructureInstance.insert("key1", "value1");
+		
+		if (dataStructureInstance.remove("Not here")) {
+			fail("Remove method did not return false when key not present");
+		}
+	}
 
 	
 	// TODO: add tests to ensure that you can detect implementation that fail
